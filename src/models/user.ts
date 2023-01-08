1bf704133330e5196e6a46a1cc07de087da7078f
ob <oberezhinskiy@gmail.com>
2023-01-08 20:26:43 +0300

    1

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
-
+import isEmail from 'validator/lib/isEmail';
 
 interface IUser {
     name: string;
     about: string;
     avatar: string;
+    email: string;
+    password: string;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
@@ -23,6 +25,20 @@ const userSchema = new mongoose.Schema<IUser>({
     avatar: {
         type: String,
         required: true
-    }
+    },
+    email: {
+        type: String,
+        minlength: 2,
+        maxlength: 30,
+        validate: { validator(s: string) { return isEmail(s) }, message: 'invalid email' },
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        minlength: 2,
+        maxlength: 30,
+        required: true
+    },
 });
 export default mongoose.model<IUser>('user', userSchema); 
